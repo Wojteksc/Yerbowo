a6cebb5e0c96da6cd177eae85a47e99c3e676b7d
Wojciech Ragan <wojteksc21@gmail.com>
2021-09-01 19:12:24 +0200

    Fix Tests

--- YerbowoBackend/tests/Yerbowo.Functional.Tests/Web/Controllers/AuthControllerTest.cs
+++ YerbowoBackend/tests/Yerbowo.Functional.Tests/Web/Controllers/AuthControllerTest.cs
@@ -2,9 +2,9 @@
 using Newtonsoft.Json;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Priority;
 using Yerbowo.Application.Auth;
 using Yerbowo.Application.Auth.Login;
 using Yerbowo.Application.Auth.Register;
@@ -13,6 +13,7 @@ using Yerbowo.Functional.Tests.Web.Helpers;
 
 namespace Yerbowo.Functional.Tests.Web.Controllers
 {
+    [TestCaseOrderer(PriorityOrderer.Name, PriorityOrderer.Assembly)]
 	public class AuthControllerTest : IClassFixture<WebTestFixture>
 	{
 		private readonly HttpClient _httpClient;
@@ -24,7 +25,7 @@ namespace Yerbowo.Functional.Tests.Web.Controllers
 			_httpClient = factory.CreateClient(new WebApplicationFactoryClientOptions());
 		}
 
-		[Fact]
+		[Fact, Priority(0)]
 		public async Task Register_Account_Should_Return_Status_Code_201()
 		{
 			var registerCommand = new RegisterCommand
