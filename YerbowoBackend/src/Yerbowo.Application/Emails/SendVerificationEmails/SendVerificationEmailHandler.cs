0a1983e929240bc790e67438baf727e6687d5299
Wojciech Ragan <wojteksc21@gmail.com>
2022-10-08 21:51:34 +0200

    Hotfix

--- YerbowoBackend/src/Yerbowo.Application/Emails/SendVerificationEmails/SendVerificationEmailHandler.cs
+++ YerbowoBackend/src/Yerbowo.Application/Emails/SendVerificationEmails/SendVerificationEmailHandler.cs
@@ -5,25 +5,25 @@ namespace Yerbowo.Application.Emails.SendVerificationEmails;
 public class SendVerificationEmailHandler : IRequestHandler<SendVerificationEmailCommand>
 {
     private readonly IVerificationEmailTemplateSender _verificationEmailTemplateSender;
-    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly HttpRequest _httpRequest;
 
     public SendVerificationEmailHandler(
         IVerificationEmailTemplateSender verificationEmailTemplateSender,
         IHttpContextAccessor httpContextAccessor)
     {
         _verificationEmailTemplateSender = verificationEmailTemplateSender;
-        _httpContextAccessor = httpContextAccessor;
+        _httpRequest = httpContextAccessor.HttpContext.Request;
     }
 
     public async Task<Unit> Handle(SendVerificationEmailCommand command, CancellationToken cancellationToken)
     {
         var user = command.User;
-        var clientUrl = _httpContextAccessor.HttpContext.Request.Headers["Referer"];
+        var domainUrl = $"{_httpRequest.Scheme}://{_httpRequest.Host.Value}";
 
         object dynamicTemplateData = new
         {
             RecipientName = command.User.FirstName,
-            ConfirmationLink = $"{clientUrl}/confirmEmail?email={user.Email}&token={user.VerificationToken}"
+            ConfirmationLink = $"{domainUrl}/confirmEmail?email={user.Email}&token={user.VerificationToken}"
         };
 
         var responseEmail = await _verificationEmailTemplateSender.SendEmailAsync(
