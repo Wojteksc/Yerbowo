cc9661690c43eb29b8e733619b1512f39d80685b
Wojciech Ragan <wojteksc21@gmail.com>
2022-11-13 13:45:22 +0100

    To revert 2

--- YerbowoBackend/src/Yerbowo.Api/Startup.cs
+++ YerbowoBackend/src/Yerbowo.Api/Startup.cs
@@ -31,20 +31,27 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env, YerbowoContextSeed dbInitializer)
     {
-        if (env.IsDevelopment())
+        if (env.IsDevelopment() && env.IsEnvironment("Testing"))
         {
+            Log.Information("UseDeveloperExceptionPage Start");
             app.UseDeveloperExceptionPage();
+            Log.Information("UseDeveloperExceptionPage End");
+            Log.Information("UseSwaggers Start");
             app.UseSwaggers();
+            Log.Information("UseSwaggers End");
         }
         else
         {
             Log.Information("UseForwardedHeadersOptions Start");
             app.UseForwardedHeadersOptions();
             Log.Information("UseForwardedHeadersOptions End");
+            Log.Information("UseHttpsRedirection Start");
             app.UseHttpsRedirection();
+            Log.Information("UseHttpsRedirection End");
         }
-
+        Log.Information("UseExceptionHandlers Start");
         app.UseExceptionHandlers();
+        Log.Information("UseExceptionHandlers End");
         app.UseCorsOptions(Configuration);
         app.UseRouting();
         app.UseSecurityHeaders();
