4c357ebdc8e17d515206a3ad7043a34a10bb69a7
Wojciech Ragan <wojciech.ragan@outlook.com>
2023-06-04 20:38:20 +0200

    Remove Yerbowo.Integration.Tests and Yerbowo.Fakers

--- YerbowoBackend/src/Yerbowo.Fakers/Extensions/CustomFakerExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yerbowo.Fakers.Extensions;
-
-public static class CustomFakerExtensions
-{
-    public static Faker<T> UsePrivateConstructor<T>(this Faker<T> faker) where T : class
-    {
-        return faker.CustomInstantiator(f => Activator.CreateInstance(typeof(T), nonPublic: true) as T);
-    }
-}
--- YerbowoBackend/src/Yerbowo.Fakers/GlobalUsings.cs
+++
@@ -1,2 +0,0 @@
-﻿global using Bogus;
-global using System;
