da08d84e1528b702b705dcc5e3250ebfc3a4b71b
Wojciech Ragan <wojciech.ragan@outlook.com>
2025-07-06 18:24:59 +0200

    Migrate to environment variables

--- YerbowoBackend/src/Yerbowo.Api/Extensions/ConfigurationBuilderExtensions/AzureKeyVaultExtensions.cs
+++ YerbowoBackend/src/Yerbowo.Api/Extensions/ConfigurationBuilderExtensions/AzureKeyVaultExtensions.cs
@@ -4,30 +4,19 @@ public static class AzureKeyVaultExtensions
 {
     public static void AddAzureKeyVault(this IConfigurationBuilder config)
     {
-        //var buildConfiguration = config.Build();
+        string kvURL = Environment.GetEnvironmentVariable("AZURE_KEYVAULT_URL");
+        string tenantId = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
+        string clientId = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
+        string clientSecret = Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET");
 
-        //string kvURL = Environment.GetEnvironmentVariable("KeyVaultConfig__KVUrl") ?? buildConfiguration["KeyVaultConfig:KVUrl"];
-        //string tenantId = Environment.GetEnvironmentVariable("KeyVaultConfig__TenantId") ?? buildConfiguration["KeyVaultConfig:TenantId"];
-        //string clientId = Environment.GetEnvironmentVariable("KeyVaultConfig__ClientId") ?? buildConfiguration["KeyVaultConfig:ClientId"];
-        //string clientSecret = Environment.GetEnvironmentVariable("KeyVaultConfig__ClientSecret") ?? buildConfiguration["KeyVaultConfig:ClientSecret"];
+        if (string.IsNullOrEmpty(kvURL)
+            || string.IsNullOrEmpty(tenantId)
+            || string.IsNullOrEmpty(clientId)
+            || string.IsNullOrEmpty(clientSecret))
+            throw new Exception("Azure Key Vault environment variables are not configured!");
 
-        //if (string.IsNullOrEmpty(kvURL) 
-        //    || string.IsNullOrEmpty(tenantId) 
-        //    || string.IsNullOrEmpty(clientId) 
-        //    || string.IsNullOrEmpty(clientSecret))
-        //    throw new Exception("Azure Key Vault settings are not configured!");
-
-
-        //var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
-        //var client = new SecretClient(new Uri(kvURL), credential);
-        //config.AddAzureKeyVault(client, new AzureKeyVaultConfigurationOptions());
-
-        var buildConfiguration = config.Build();
-        string kvUrl = Environment.GetEnvironmentVariable("KVUrl") ?? buildConfiguration["KeyVaultConfig:KVUrl"];
-        if (string.IsNullOrEmpty(kvUrl))
-            throw new Exception("Azure Key Vault settings are not configured!");
-
-        var client = new SecretClient(new Uri(kvUrl), new DefaultAzureCredential());
+        var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+        var client = new SecretClient(new Uri(kvURL), credential);
         config.AddAzureKeyVault(client, new AzureKeyVaultConfigurationOptions());
     }
 }
